348880f4b6fe99b3d66719d64e6385e8fda8eebe
Sassyprogrammer1 <hamzajamshed.cs@gmail.com>
2022-09-26 21:52:58 +0500

    update readme

---
+++ workerThread.js
@@ -0,0 +1,4 @@
+// Node.js is an ideal choice to build I/O intensive applications
+// However when it comes to CPU intensive taks mostly node.js lacks performance.
+// For such scenarios we consider using worker threads in node.js which basically 
+// Divides the CPU intensive workloads between multiple threads.
