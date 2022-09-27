e874d84825a87c1b6bea814e13ec4861ef5147e2
Sassyprogrammer1 <hamzajamshed.cs@gmail.com>
2022-09-27 23:32:58 +0500

    updated readme

--- workerThread.js
+++ workerThread.js
@@ -2,3 +2,6 @@
 // However when it comes to CPU intensive taks mostly node.js lacks performance.
 // For such scenarios we consider using worker threads in node.js which basically 
 // Divides the CPU intensive workloads between multiple threads.
+
+
+// initiate a worker thread
